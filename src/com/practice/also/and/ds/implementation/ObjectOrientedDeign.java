9361de53811db558e247a8e2ff1615459025f900
Saurav Kakar <saurabh.kakar@priceline.com>
2017-05-11 17:03:17 -0400

    deleted also

--- src/com/practice/also/and/ds/implementation/ObjectOrientedDeign.java
+++
@@ -1,9 +0,0 @@
-package com.practice.also.and.ds.implementation;
-
-public class ObjectOrientedDeign {
-	public static void main(String[] args) {
-		ObjectOrientedAnalysisDesign o = new ObjectOrientedAnalysisDesign();
-		o.printNameProtected();
-	}
-
-}
